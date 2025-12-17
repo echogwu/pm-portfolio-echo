acc912bf1ba212c9a4bdfdb54fe20f980a0192b7
Echo Wu <echo.g.wu@gmail.com>
2025-12-16 21:34:11 -0800

    attempt to fix GTM integration part 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,6 +72,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Marker so you can verify via View Source */}
+        <meta name="gtm-container" content={gtmId} />
+
+        {/* Google Tag Manager */}
+        {gtmId ? (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`,
+            }}
+          />
+        ) : null}
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>
         {/* Google Tag Manager (noscript) */}
         {gtmId ? (
